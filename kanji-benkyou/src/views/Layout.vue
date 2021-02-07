fbef46c7731917c08fe0e848d79a2ad5692122ca
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2021-02-07 22:54:13 +0100

    Change background color on layout

--- kanji-benkyou/src/views/Layout.vue
+++ kanji-benkyou/src/views/Layout.vue
@@ -1,41 +1,41 @@
 <template>
   <a-layout>
-      <a-menu
-        mode="horizontal"
-        :selectedKeys="[$route.name]"
-        theme="dark"
-      >
-        <a-menu-item key="Home">
-          <router-link :to="{ name: 'Home' }">
-            <home-outlined />
-            Home
-          </router-link>
-        </a-menu-item>
+    <a-menu mode="horizontal" :selectedKeys="[$route.name]" theme="dark">
+      <a-menu-item key="Home">
+        <router-link :to="{ name: 'Home' }">
+          <home-outlined />
+          Home
+        </router-link>
+      </a-menu-item>
 
-        <a-menu-item key="JLPT">
-          <router-link :to="{ name: 'JLPT' }">
-            <file-protect-outlined />
-            JLPT
-          </router-link>
-        </a-menu-item>
+      <a-menu-item key="JLPT">
+        <router-link :to="{ name: 'JLPT' }">
+          <file-protect-outlined />
+          JLPT
+        </router-link>
+      </a-menu-item>
 
-        <a-menu-item key="Decks" v-if="user.isLogged">
-          <router-link :to="{ name: 'Decks' }">
-            <book-outlined />
-            Decks
-          </router-link>
-        </a-menu-item>
+      <a-menu-item key="Decks" v-if="user.isLogged">
+        <router-link :to="{ name: 'Decks' }">
+          <book-outlined />
+          Decks
+        </router-link>
+      </a-menu-item>
 
-        <a-menu-item key="Login" v-if="!user.isLogged">
-          <router-link :to="{ name: 'Login' }">
-            <user-outlined />
-            Login
-          </router-link>
-        </a-menu-item>
-      </a-menu>
+      <a-menu-item key="Login" v-if="!user.isLogged">
+        <router-link :to="{ name: 'Login' }">
+          <user-outlined />
+          Login
+        </router-link>
+      </a-menu-item>
+    </a-menu>
 
     <a-layout-content
-      :style="{ overflow: 'auto', minHeight: 'calc(100vh - 117px)' }"
+      :style="{
+        overflow: 'auto',
+        minHeight: 'calc(100vh - 117px)',
+        backgroundColor: 'white',
+      }"
     >
       <slot />
     </a-layout-content>
